e1e4160151fd8d02da43988c6104fb0f0a759d22
Fabricio Henrique da Silva <fabricionhsilva@gmail.com>
2025-11-16 17:04:28 -0300

    fix: correção no nome do campo do dto

--- backend/src/main/java/br/com/bookmanager/domain/assunto/dto/AssuntoResponseDTO.java
+++ backend/src/main/java/br/com/bookmanager/domain/assunto/dto/AssuntoResponseDTO.java
@@ -3,7 +3,7 @@ package br.com.bookmanager.domain.assunto.dto;
 import br.com.bookmanager.domain.assunto.model.Assunto;
 
 public record AssuntoResponseDTO(Integer codAs,
-                                 String nome) {
+                                 String descricao) {
 
     public AssuntoResponseDTO(Assunto assunto) {
         this(assunto.getCodAs(), assunto.getDescricao());
